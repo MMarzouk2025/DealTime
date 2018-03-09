a87b85fa27d2482eb66a2a366d5bb491c9b85040
ahmed-mokhtar <31440751+ahmed-mokhtar@users.noreply.github.com>
2018-03-09 03:42:28 +0200

    Fix html5 validation, Add header&footer

--- web/res/js/customer.profile.js
+++ web/res/js/customer.profile.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
+    
     $("#edit_profile_btn").click(function () {
+        
         $(':input[type="text"]').prop('disabled', false);
         $(':input[type="password"]').prop('disabled', false);
         $(':input[type="date"]').prop('disabled', false);
@@ -10,8 +12,20 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
+    
     $("#save_changes_btn").click(function () {
 
         $("#edit_profile_btn").prop('disabled', false);
     });
 });
+
+$(window).load(function () {
+    
+    $(':input[type="text"]').prop('disabled', true);
+    $(':input[type="password"]').prop('disabled', true);
+    $(':input[type="date"]').prop('disabled', true);
+    
+    $("#save_changes_btn").prop('disabled', true);
+    $("#edit_profile_btn").prop('disabled', false);
+});
+
